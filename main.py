fc68e52052260d9186855cf43047029f12f51705
DhruvKAggarwal123 <dhruv0130.becse24@chitkara.edu.in>
2025-09-14 17:13:05 +0530

    main

--- main.py
+++ main.py
@@ -139,5 +139,9 @@ async def simplify(question: str = Form(...)):
 # -------------------------
 # Run app
 # -------------------------
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Render provides PORT
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
